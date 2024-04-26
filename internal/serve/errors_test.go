c361ef59348215de3960787107766e39cb0d1e7a
Howard Chen <howard@stellar.org>
2024-04-25 17:49:55 -0700

    make test more extensive

--- internal/serve/errors_test.go
+++ internal/serve/errors_test.go
@@ -1,6 +1,7 @@
 package serve
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,23 +12,66 @@ import (
 )
 
 func TestErrorResponseRender(t *testing.T) {
-	w := httptest.NewRecorder()
-	serverError.Render(w)
-	resp := w.Result()
-	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
-	body, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-	assert.JSONEq(t, `{"error":"An error occurred while processing this request."}`, string(body))
+	testCases := []struct {
+		in   errorResponse
+		want errorResponse
+	}{
+		{
+			in:   serverError,
+			want: errorResponse{Status: http.StatusInternalServerError, Error: "An error occurred while processing this request."},
+		},
+		{
+			in:   notFound,
+			want: errorResponse{Status: http.StatusNotFound, Error: "The resource at the url requested was not found."},
+		},
+		{
+			in:   methodNotAllowed,
+			want: errorResponse{Status: http.StatusMethodNotAllowed, Error: "The method is not allowed for resource at the url requested."},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			w := httptest.NewRecorder()
+			tc.in.Render(w)
+			resp := w.Result()
+			assert.Equal(t, tc.want.Status, resp.StatusCode)
+			body, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, fmt.Sprintf(`{"error":%q}`, tc.want.Error), string(body))
+		})
+	}
 }
 
 func TestErrorHandler(t *testing.T) {
-	r := httptest.NewRequest("GET", "/404", nil)
-	w := httptest.NewRecorder()
-	handler := errorHandler{Error: notFound}
-	handler.ServeHTTP(w, r)
-	resp := w.Result()
-	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
-	body, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-	assert.JSONEq(t, `{"error":"The resource at the url requested was not found."}`, string(body))
+	testCases := []struct {
+		in   errorHandler
+		want errorResponse
+	}{
+		{
+			in:   errorHandler{serverError},
+			want: errorResponse{Status: http.StatusInternalServerError, Error: "An error occurred while processing this request."},
+		},
+		{
+			in:   errorHandler{notFound},
+			want: errorResponse{Status: http.StatusNotFound, Error: "The resource at the url requested was not found."},
+		},
+		{
+			in:   errorHandler{methodNotAllowed},
+			want: errorResponse{Status: http.StatusMethodNotAllowed, Error: "The method is not allowed for resource at the url requested."},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/", nil)
+			w := httptest.NewRecorder()
+			tc.in.ServeHTTP(w, r)
+			resp := w.Result()
+			assert.Equal(t, tc.want.Status, resp.StatusCode)
+			body, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, fmt.Sprintf(`{"error":%q}`, tc.want.Error), string(body))
+		})
+	}
 }
