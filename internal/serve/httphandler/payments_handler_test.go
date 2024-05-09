9f3948e8f0c77174163e6f5809444d0682a67bcb
daniel <40469266+daniel-burghardt@users.noreply.github.com>
2024-05-09 15:25:42 -0300

    test 4

--- internal/serve/httphandler/payments_handler_test.go
+++ internal/serve/httphandler/payments_handler_test.go
@@ -20,10 +20,10 @@ import (
 )
 
 func TestSubscribeAddress(t *testing.T) {
-	dbtest := dbtest.Open(t)
-	defer dbtest.Close()
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
 
-	dbConnectionPool, err := db.OpenDBConnectionPool(dbtest.DSN)
+	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
 	require.NoError(t, err)
 	defer dbConnectionPool.Close()
 
@@ -101,10 +101,10 @@ func TestSubscribeAddress(t *testing.T) {
 }
 
 func TestUnsubscribeAddress(t *testing.T) {
-	dbtest := dbtest.Open(t)
-	defer dbtest.Close()
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
 
-	dbConnectionPool, err := db.OpenDBConnectionPool(dbtest.DSN)
+	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
 	require.NoError(t, err)
 	defer dbConnectionPool.Close()
 
