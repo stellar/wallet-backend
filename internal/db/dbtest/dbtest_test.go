169ee3bede88b6893a56dc660aee8b28cc7d9977
daniel <40469266+daniel-burghardt@users.noreply.github.com>
2024-05-09 14:58:44 -0300

    test 2

--- internal/db/dbtest/dbtest_test.go
+++ internal/db/dbtest/dbtest_test.go
@@ -1,13 +1,20 @@
 package dbtest
 
-// func TestOpen(t *testing.T) {
-// 	db := Open(t)
-// 	defer db.Close()
-// 	conn := db.Open()
-// 	defer conn.Close()
+import (
+	"testing"
 
-// 	count := 0
-// 	err := conn.Get(&count, `SELECT COUNT(*) FROM gorp_migrations`)
-// 	require.NoError(t, err)
-// 	assert.Greater(t, count, 0)
-// }
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpen(t *testing.T) {
+	db := Open(t)
+	defer db.Close()
+	conn := db.Open()
+	defer conn.Close()
+
+	count := 0
+	err := conn.Get(&count, `SELECT COUNT(*) FROM gorp_migrations`)
+	require.NoError(t, err)
+	assert.Greater(t, count, 0)
+}
