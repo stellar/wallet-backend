4fc073fd18c1300dd9afa3a3817eeb19f97d7943
Daniel Burghardt <danielburghardt2@gmail.com>
2024-06-27 12:24:08 -0300

    ingest: asset_types and memo_type columns added

--- internal/data/payments_test.go
+++ internal/data/payments_test.go
@@ -2,9 +2,11 @@ package data
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
+	"github.com/stellar/go/xdr"
 	"github.com/stellar/wallet-backend/internal/db"
 	"github.com/stellar/wallet-backend/internal/db/dbtest"
 	"github.com/stretchr/testify/assert"
@@ -30,19 +32,22 @@ func TestPaymentModelAddPayment(t *testing.T) {
 	)
 	payment := Payment{
 		OperationID:     2120562792996865,
-		OperationType:   "OperationTypePayment",
+		OperationType:   xdr.OperationTypePayment.String(),
 		TransactionID:   2120562792996864,
 		TransactionHash: "a3daffa64dc46db84888b1206dc8014a480042e7fe8b19fd5d05465709f4e887",
 		FromAddress:     fromAddress,
 		ToAddress:       toAddress,
 		SrcAssetCode:    "USDC",
 		SrcAssetIssuer:  "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5",
+		SrcAssetType:    xdr.AssetTypeAssetTypeCreditAlphanum4.String(),
 		SrcAmount:       500000000,
 		DestAssetCode:   "USDC",
 		DestAssetIssuer: "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5",
+		DestAssetType:   xdr.AssetTypeAssetTypeCreditAlphanum4.String(),
 		DestAmount:      500000000,
 		CreatedAt:       time.Date(2023, 12, 15, 1, 0, 0, 0, time.UTC),
 		Memo:            nil,
+		MemoType:        xdr.MemoTypeMemoNone.String(),
 	}
 
 	addPayment := func() {
@@ -52,12 +57,10 @@ func TestPaymentModelAddPayment(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	fetchPaymentInserted := func() bool {
-		var inserted bool
-		err := dbConnectionPool.QueryRowxContext(ctx, "SELECT EXISTS(SELECT 1 FROM ingest_payments)").Scan(&inserted)
-		require.NoError(t, err)
-
-		return inserted
+	fetchPayment := func() (Payment, error) {
+		var dbPayment Payment
+		err := dbConnectionPool.GetContext(ctx, &dbPayment, "SELECT * FROM ingest_payments")
+		return dbPayment, err
 	}
 
 	cleanUpDB := func() {
@@ -68,8 +71,8 @@ func TestPaymentModelAddPayment(t *testing.T) {
 	t.Run("unkown_address", func(t *testing.T) {
 		addPayment()
 
-		inserted := fetchPaymentInserted()
-		assert.False(t, inserted)
+		_, err := fetchPayment()
+		assert.ErrorIs(t, err, sql.ErrNoRows)
 
 		cleanUpDB()
 	})
@@ -80,8 +83,9 @@ func TestPaymentModelAddPayment(t *testing.T) {
 
 		addPayment()
 
-		inserted := fetchPaymentInserted()
-		assert.True(t, inserted)
+		dbPayment, err := fetchPayment()
+		require.NoError(t, err)
+		assert.Equal(t, payment, dbPayment)
 
 		cleanUpDB()
 	})
@@ -92,8 +96,9 @@ func TestPaymentModelAddPayment(t *testing.T) {
 
 		addPayment()
 
-		inserted := fetchPaymentInserted()
-		assert.True(t, inserted)
+		dbPayment, err := fetchPayment()
+		require.NoError(t, err)
+		assert.Equal(t, payment, dbPayment)
 
 		cleanUpDB()
 	})
@@ -146,7 +151,7 @@ func TestPaymentModelUpdateLatestLedgerSynced(t *testing.T) {
 	assert.Equal(t, uint32(123), lastSyncedLedger)
 }
 
-func TestPaymentModelGetPayments(t *testing.T) {
+func TestPaymentModelGetPaymentsPaginated(t *testing.T) {
 	dbt := dbtest.Open(t)
 	defer dbt.Close()
 	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
@@ -159,11 +164,11 @@ func TestPaymentModelGetPayments(t *testing.T) {
 	}
 
 	dbPayments := []Payment{
-		{OperationID: 1, OperationType: "OperationTypePayment", TransactionID: 11, TransactionHash: "c370ff20144e4c96b17432b8d14664c1", FromAddress: "GAZ37ZO4TU3H", ToAddress: "GDD2HQO6IOFT", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAmount: 10, DestAssetCode: "XLM", DestAssetIssuer: "", DestAmount: 10, CreatedAt: time.Date(2024, 6, 21, 0, 0, 0, 0, time.UTC), Memo: nil},
-		{OperationID: 2, OperationType: "OperationTypePayment", TransactionID: 22, TransactionHash: "30850d8fc7d1439782885103390cd975", FromAddress: "GBZ5Q56JKHJQ", ToAddress: "GASV72SENBSY", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAmount: 20, DestAssetCode: "XLM", DestAssetIssuer: "", DestAmount: 20, CreatedAt: time.Date(2024, 6, 22, 0, 0, 0, 0, time.UTC), Memo: nil},
-		{OperationID: 3, OperationType: "OperationTypePayment", TransactionID: 33, TransactionHash: "d9521ed7057d4d1e9b9dd22ab515cbf1", FromAddress: "GAYFAYPOECBT", ToAddress: "GDWDPNMALNIT", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAmount: 30, DestAssetCode: "XLM", DestAssetIssuer: "", DestAmount: 30, CreatedAt: time.Date(2024, 6, 23, 0, 0, 0, 0, time.UTC), Memo: nil},
-		{OperationID: 4, OperationType: "OperationTypePayment", TransactionID: 44, TransactionHash: "2af98496a86741c6a6814200e06027fd", FromAddress: "GACKTNR2QQXU", ToAddress: "GBZ5KUZHAAVI", SrcAssetCode: "USDC", SrcAssetIssuer: "GAHLU7PDIQMZ", SrcAmount: 40, DestAssetCode: "USDC", DestAssetIssuer: "GAHLU7PDIQMZ", DestAmount: 40, CreatedAt: time.Date(2024, 6, 24, 0, 0, 0, 0, time.UTC), Memo: nil},
-		{OperationID: 5, OperationType: "OperationTypePayment", TransactionID: 55, TransactionHash: "edfab36f9f104c4fb74b549de44cfbcc", FromAddress: "GA4CMYJEC5W5", ToAddress: "GAZ37ZO4TU3H", SrcAssetCode: "USDC", SrcAssetIssuer: "GAHLU7PDIQMZ", SrcAmount: 50, DestAssetCode: "USDC", DestAssetIssuer: "GAHLU7PDIQMZ", DestAmount: 50, CreatedAt: time.Date(2024, 6, 25, 0, 0, 0, 0, time.UTC), Memo: nil},
+		{OperationID: 1, OperationType: xdr.OperationTypePayment.String(), TransactionID: 11, TransactionHash: "c370ff20144e4c96b17432b8d14664c1", FromAddress: "GAZ37ZO4TU3H", ToAddress: "GDD2HQO6IOFT", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAssetType: xdr.AssetTypeAssetTypeNative.String(), SrcAmount: 10, DestAssetCode: "XLM", DestAssetIssuer: "", DestAssetType: xdr.AssetTypeAssetTypeNative.String(), DestAmount: 10, CreatedAt: time.Date(2024, 6, 21, 0, 0, 0, 0, time.UTC), Memo: nil, MemoType: xdr.MemoTypeMemoNone.String()},
+		{OperationID: 2, OperationType: xdr.OperationTypePayment.String(), TransactionID: 22, TransactionHash: "30850d8fc7d1439782885103390cd975", FromAddress: "GBZ5Q56JKHJQ", ToAddress: "GASV72SENBSY", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAssetType: xdr.AssetTypeAssetTypeNative.String(), SrcAmount: 20, DestAssetCode: "XLM", DestAssetIssuer: "", DestAssetType: xdr.AssetTypeAssetTypeNative.String(), DestAmount: 20, CreatedAt: time.Date(2024, 6, 22, 0, 0, 0, 0, time.UTC), Memo: nil, MemoType: xdr.MemoTypeMemoNone.String()},
+		{OperationID: 3, OperationType: xdr.OperationTypePayment.String(), TransactionID: 33, TransactionHash: "d9521ed7057d4d1e9b9dd22ab515cbf1", FromAddress: "GAYFAYPOECBT", ToAddress: "GDWDPNMALNIT", SrcAssetCode: "XLM", SrcAssetIssuer: "", SrcAssetType: xdr.AssetTypeAssetTypeNative.String(), SrcAmount: 30, DestAssetCode: "XLM", DestAssetIssuer: "", DestAssetType: xdr.AssetTypeAssetTypeNative.String(), DestAmount: 30, CreatedAt: time.Date(2024, 6, 23, 0, 0, 0, 0, time.UTC), Memo: nil, MemoType: xdr.MemoTypeMemoNone.String()},
+		{OperationID: 4, OperationType: xdr.OperationTypePayment.String(), TransactionID: 44, TransactionHash: "2af98496a86741c6a6814200e06027fd", FromAddress: "GACKTNR2QQXU", ToAddress: "GBZ5KUZHAAVI", SrcAssetCode: "USDC", SrcAssetIssuer: "GAHLU7PDIQMZ", SrcAssetType: xdr.AssetTypeAssetTypeCreditAlphanum4.String(), SrcAmount: 40, DestAssetCode: "USDC", DestAssetIssuer: "GAHLU7PDIQMZ", DestAssetType: xdr.AssetTypeAssetTypeCreditAlphanum4.String(), DestAmount: 40, CreatedAt: time.Date(2024, 6, 24, 0, 0, 0, 0, time.UTC), Memo: nil, MemoType: xdr.MemoTypeMemoNone.String()},
+		{OperationID: 5, OperationType: xdr.OperationTypePayment.String(), TransactionID: 55, TransactionHash: "edfab36f9f104c4fb74b549de44cfbcc", FromAddress: "GA4CMYJEC5W5", ToAddress: "GAZ37ZO4TU3H", SrcAssetCode: "USDC", SrcAssetIssuer: "GAHLU7PDIQMZ", SrcAssetType: xdr.AssetTypeAssetTypeCreditAlphanum4.String(), SrcAmount: 50, DestAssetCode: "USDC", DestAssetIssuer: "GAHLU7PDIQMZ", DestAssetType: xdr.AssetTypeAssetTypeCreditAlphanum4.String(), DestAmount: 50, CreatedAt: time.Date(2024, 6, 25, 0, 0, 0, 0, time.UTC), Memo: nil, MemoType: xdr.MemoTypeMemoNone.String()},
 	}
 	InsertTestPayments(t, ctx, dbPayments, dbConnectionPool)
 
