9920f48ea88a6094ff4e5b52708b48da4c87c137
gouthamp-stellar <goutham.patnaikuni@stellar.org>
2024-08-30 15:06:40 -0700

    TSS tables and the channel interface

--- internal/db/migrations/2024-08-28.0-tss_transactions.sql
+++ internal/db/migrations/2024-08-28.0-tss_transactions.sql
@@ -1,6 +1,7 @@
 -- +migrate Up
+
 CREATE TABLE tss_transactions (
-    transaction_id VARCHAR(400) PRIMARY KEY,
+    transaction_xdr VARCHAR(400) PRIMARY KEY,
     webhook_url VARCHAR(250),
     current_status VARCHAR(50),
     creation_time TIMESTAMPTZ DEFAULT NOW(),
@@ -12,9 +13,11 @@ CREATE INDEX idx_tx_current_status ON tss_transactions(current_status);
 CREATE INDEX idx_claimed_until ON tss_transactions(claimed_until);
 
 -- +migrate Down
+
 DROP INDEX IF EXISTS idx_tx_current_status;
 DROP INDEX IF EXISTS idx_claimed_until;
 DROP TABLE tss_transactions
+
 
 
 
--- internal/tss/channel.go
+++ internal/tss/channel.go
@@ -1,30 +1,34 @@
 package tss
 
 type RPCIngestTxResponse struct {
-	// the raw TransactionEnvelope XDR of the transaction corresponding to the sendTransaction call that returned a PENDING status
-	envelopeXdr string
+	// a status that indicated whether this transaction failed or successly made it to the ledger
+	Status string
+	// the raw TransactionEnvelope XDR for this transaction
+	EnvelopeXdr string
 	// the raw TransactionResult XDR of the envelopeXdr
-	resultXdr string
+	ResultXdr string
+	// The unix timestamp of when the transaction was included in the ledger
+	CreatedAt int64
 }
 
 type RPCSendTxResponse struct {
 	// the status of an RPC sendTransaction call. Can be one of [PENDING, DUPLICATE, TRY_AGAIN_LATER, ERROR]
-	status string
-	/*
-		the (optional) error code that is derived by deserialzing the errorResultXdr string in the sendTransaction response
-		list of possible errror codes: https://developers.stellar.org/docs/data/horizon/api-reference/errors/result-codes/transactions
-	*/
-	errorCode string
+	Status string
+	// the (optional) error code that is derived by deserialzing the errorResultXdr string in the sendTransaction response
+	// list of possible errror codes: https://developers.stellar.org/docs/data/horizon/api-reference/errors/result-codes/transactions
+	ErrorCode string
 }
 
-type RPCPayload struct {
+type Payload struct {
 	// the transaction xdr submitted by the client
-	transactionId       string
-	rpcSubmitTxResponse RPCSendTxResponse
-	rpcIngestTxResponse RPCIngestTxResponse
+	TransactionXdr string
+	// relevant fields in an RPC sendTransaction response
+	RpcSubmitTxResponse RPCSendTxResponse
+	// relevant fields in the transaction list inside the RPC getTransactions response
+	RpcIngestTxResponse RPCIngestTxResponse
 }
 
 type Channel interface {
-	send(payload RPCPayload)
-	receive(payload RPCPayload)
+	send(payload Payload)
+	receive(payload Payload)
 }
