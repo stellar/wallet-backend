fe5128bc4b52d64e65fc27e780bf3f316ae2518d
gouthamp-stellar <goutham.patnaikuni@stellar.org>
2024-09-13 02:07:50 -0700

    commit latest changes

--- internal/db/migrations/2024-08-28.0-tss_transactions.sql
+++ internal/db/migrations/2024-08-28.0-tss_transactions.sql
@@ -11,10 +11,10 @@ CREATE TABLE tss_transactions (
 );
 
 CREATE TABLE tss_transaction_submission_tries (
+    try_transaction_hash TEXT PRIMARY KEY,
     original_transaction_hash TEXT,
-    try_transaction_hash TEXT,
     try_transaction_xdr TEXT,
-    status TEXT,
+    status INTEGER,
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
@@ -22,8 +22,7 @@ CREATE INDEX idx_tx_current_status ON tss_transactions(current_status);
 CREATE INDEX idx_claimed_until ON tss_transactions(claimed_until);
 
 CREATE INDEX idx_original_transaction_hash ON tss_transaction_submission_tries(original_transaction_hash);
-CREATE INDEX idx_try_transaction_hash ON tss_transaction_submission_tries(try_transaction_hash);
-CREATE INDEX idx_last_updated ON tss_transaction_submission_tries(last_updated);
+CREATE INDEX idx_last_updated ON tss_transaction_submission_tries(updated_at);
 
 -- +migrate Down
 
@@ -32,6 +31,5 @@ DROP INDEX IF EXISTS idx_claimed_until;
 DROP TABLE tss_transactions;
 
 DROP INDEX IF EXISTS idx_original_transaction_hash;
-DROP INDEX IF EXISTS idx_try_transaction_hash;
 DROP INDEX IF EXISTS idx_last_updated;
 DROP TABLE tss_transaction_submission_tries;
--- internal/tss/types.go
+++ internal/tss/types.go
@@ -1,8 +1,29 @@
 package tss
 
+import "github.com/stellar/go/xdr"
+
 type RPCTXStatus string
+type OtherCodes int32
+
+type TransactionResultCode int32
 
 const (
+	// Do not use NoCode
+	NoCode OtherCodes = 0
+	// These values need to not overlap the values in xdr.TransactionResultCode
+	NewCode             OtherCodes = 100
+	RPCFailCode         OtherCodes = 101
+	UnMarshalBinaryCode OtherCodes = 102
+)
+
+type RPCTXCode struct {
+	TxResultCode xdr.TransactionResultCode
+	OtherCodes   OtherCodes
+}
+
+const (
+	// Brand new transaction, not sent to RPC yet
+	NewStatus RPCTXStatus = "NEW"
 	// RPC sendTransaction statuses
 	PendingStatus       RPCTXStatus = "PENDING"
 	DuplicateStatus     RPCTXStatus = "DUPLICATE"
@@ -26,16 +47,22 @@ type RPCGetIngestTxResponse struct {
 }
 
 type RPCSendTxResponse struct {
+	// The hash of the transaction submitted to RPC
+	TransactionHash string
+	TransactionXDR  string
 	// The status of an RPC sendTransaction call. Can be one of [PENDING, DUPLICATE, TRY_AGAIN_LATER, ERROR]
 	Status RPCTXStatus
 	// The (optional) error code that is derived by deserialzing the errorResultXdr string in the sendTransaction response
 	// list of possible errror codes: https://developers.stellar.org/docs/data/horizon/api-reference/errors/result-codes/transactions
-	ErrorCode string
+	Code RPCTXCode
 }
 
 type Payload struct {
+	WebhookURL string
 	// The hash of the transaction xdr submitted by the client - the id of the transaction submitted by a client
 	TransactionHash string
+	// The xdr of the transaction
+	TransactionXDR string
 	// Relevant fields in an RPC sendTransaction response
 	RpcSubmitTxResponse RPCSendTxResponse
 	// Relevant fields in the transaction list inside the RPC getTransactions response
@@ -45,4 +72,5 @@ type Payload struct {
 type Channel interface {
 	Send(payload Payload)
 	Receive(payload Payload)
+	Stop()
 }
