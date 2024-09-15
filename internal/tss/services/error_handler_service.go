d32adb1563257a0062653b3ea6145cdb8080fef0
gouthamp-stellar <goutham.patnaikuni@stellar.org>
2024-09-15 16:05:37 -0700

    error handler service returns errorHandlerService

--- internal/tss/services/error_handler_service.go
+++ internal/tss/services/error_handler_service.go
@@ -4,18 +4,16 @@ import (
 	"github.com/stellar/wallet-backend/internal/tss"
 )
 
-// nolint:unused
 type errorHandlerService struct {
 	channel tss.Channel
 }
 
 func NewErrorHandlerService(channel tss.Channel) Service {
-	return &rpcCallerService{
+	return &errorHandlerService{
 		channel: channel,
 	}
 }
 
-// nolint:unused
 func (p *errorHandlerService) ProcessPayload(payload tss.Payload) {
 	// fill in later
 }
