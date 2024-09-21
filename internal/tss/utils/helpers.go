f10d2398f063347de177d9a85d32aa1ea6216752
gouthamp-stellar <goutham.patnaikuni@stellar.org>
2024-09-21 16:08:52 -0700

    removing BuildTestFeeBumpTransaction

--- internal/tss/utils/helpers.go
+++ internal/tss/utils/helpers.go
@@ -26,14 +26,3 @@ func BuildTestTransaction() *txnbuild.Transaction {
 	})
 	return tx
 }
-
-func BuildTestFeeBumpTransaction() *txnbuild.FeeBumpTransaction {
-
-	feeBumpTx, _ := txnbuild.NewFeeBumpTransaction(
-		txnbuild.FeeBumpTransactionParams{
-			Inner:      BuildTestTransaction(),
-			FeeAccount: keypair.MustRandom().Address(),
-			BaseFee:    110,
-		})
-	return feeBumpTx
-}
