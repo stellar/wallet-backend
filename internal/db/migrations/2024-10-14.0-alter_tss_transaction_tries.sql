f50ba231388b93fd6876a7965e8f1122ce910ead
gouthamp-stellar <goutham.patnaikuni@stellar.org>
2024-10-16 13:29:34 -0400

    fixing migrations

--- internal/db/migrations/2024-10-14.0-alter_tss_transaction_tries.sql
+++ internal/db/migrations/2024-10-14.0-alter_tss_transaction_tries.sql
@@ -1,15 +1,15 @@
 -- +migrate Up
 
 ALTER TABLE tss_transaction_submission_tries
-    ALTER COLUMN status TYPE TEXT,
-    ALTER COLUMN status SET NOT NULL,
+    DROP column status,
+    ADD column status TEXT NOT NULL,
     ADD COLUMN code INTEGER NOT NULL,
     ADD COLUMN result_xdr TEXT NOT NULL;
 
 -- +migrate Down
 
 ALTER TABLE tss_transaction_submission_tries
-    ALTER COLUMN status TYPE INTEGER,
-    ALTER COLUMN status SET NOT NULL,
+    DROP column status,
     DROP COLUMN code,
-    DROP COLUMN result_xdr;
+    DROP COLUMN result_xdr,
+    ADD column status INTEGER NOT NULL;
