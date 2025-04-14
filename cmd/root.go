5256d32d6cee2c4152cfefc9dfb98adcc27230e4
Marcelo Salloum <marcelo@stellar.org>
2025-04-14 12:41:37 -0700

    Set default log level to Trace and log version at startup of any CLI command.

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,31 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/stellar/go/support/log"
 )
+
+// Version is the official version of this application.
+const Version = "0.1.0"
+
+// GitCommit is populated at build time by
+// go build -ldflags "-X main.GitCommit=$GIT_COMMIT"
+var GitCommit string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:           "wallet-backend",
 	Short:         "Wallet Backend Server",
 	SilenceErrors: true,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		err := cmd.Help()
 		if err != nil {
-			log.Fatalf("Error calling help command: %s", err.Error())
+			return fmt.Errorf("calling help command: %w", err)
 		}
+		return nil
 	},
 }
 
@@ -23,12 +34,19 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		log.Fatalf("Error executing root command: %s", err.Error())
+		panic(fmt.Errorf("executing root command: %w", err))
 	}
 }
 
-func init() {
+func preConfigureLogger() {
 	log.DefaultLogger = log.New()
+	log.DefaultLogger.SetLevel(logrus.TraceLevel)
+}
+
+func init() {
+	preConfigureLogger()
+	log.Info("Version: ", Version)
+	log.Info("GitCommit: ", GitCommit)
 
 	rootCmd.AddCommand((&serveCmd{}).Command())
 	rootCmd.AddCommand((&ingestCmd{}).Command())
