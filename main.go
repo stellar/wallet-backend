6a3f79cb74a4cd7dcff30404de8f83495c9f78df
Marcelo Salloum <marcelo@stellar.org>
2025-04-18 13:21:42 -0700

    Print gitCommit and version in the command lines.

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"github.com/stellar/wallet-backend/cmd"
 )
 
+// Version is the official version of this application.
+const Version = "0.2.0"
+
 // GitCommit is populated at build time by
 // go build -ldflags "-X main.GitCommit=$GIT_COMMIT"
 var GitCommit string
 
 func main() {
-	cmd.Execute()
+	cmd.Execute(cmd.RootConfig{
+		GitCommit: GitCommit,
+		Version:   Version,
+	})
 }
