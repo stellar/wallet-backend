82746defc883180a902708ad420499d3dc370a31
Marcelo Salloum <marcelo@stellar.org>
2025-04-24 16:23:52 -0700

    Move txString to helpers.go

--- internal/integrationtests/helpers.go
+++ internal/integrationtests/helpers.go
@@ -31,3 +31,27 @@ func parseFeeBumpTxXDR(txXDR string) (*txnbuild.FeeBumpTransaction, error) {
 	}
 	return feeBumpTx, nil
 }
+
+// txString returns a string representation of a transaction given its XDR.
+func txString(txXDR string) (string, error) {
+	genericTx, err := txnbuild.TransactionFromXDR(txXDR)
+	if err != nil {
+		return "", fmt.Errorf("building transaction from XDR: %w", err)
+	}
+
+	opsSliceStr := func(ops []txnbuild.Operation) []string {
+		var opsStr []string
+		for _, op := range ops {
+			opsStr = append(opsStr, fmt.Sprintf("\n\t\t%#v", op))
+		}
+		return opsStr
+	}
+
+	if tx, ok := genericTx.Transaction(); ok {
+		return fmt.Sprintf("\n\ttx=%#v, \n\tops=%+v", tx, opsSliceStr(tx.Operations())), nil
+	} else if feeBumpTx, ok := genericTx.FeeBump(); ok {
+		return fmt.Sprintf("\n\tfeeBump=%#v, \n\ttx=%#v, \n\tops=%+v", feeBumpTx, feeBumpTx.InnerTransaction(), opsSliceStr(feeBumpTx.InnerTransaction().Operations())), nil
+	}
+
+	return fmt.Sprintf("%+v", genericTx), nil
+}
--- internal/integrationtests/integrationtests.go
+++ internal/integrationtests/integrationtests.go
@@ -283,30 +283,6 @@ func assertOrFail(condition bool, format string, args ...any) {
 	}
 }
 
-// txString returns a string representation of a transaction given its XDR.
-func txString(txXDR string) (string, error) {
-	genericTx, err := txnbuild.TransactionFromXDR(txXDR)
-	if err != nil {
-		return "", fmt.Errorf("building transaction from XDR: %w", err)
-	}
-
-	opsSliceStr := func(ops []txnbuild.Operation) []string {
-		var opsStr []string
-		for _, op := range ops {
-			opsStr = append(opsStr, fmt.Sprintf("\n\t\t%#v", op))
-		}
-		return opsStr
-	}
-
-	if tx, ok := genericTx.Transaction(); ok {
-		return fmt.Sprintf("\n\ttx=%#v, \n\tops=%+v", tx, opsSliceStr(tx.Operations())), nil
-	} else if feeBumpTx, ok := genericTx.FeeBump(); ok {
-		return fmt.Sprintf("\n\tfeeBump=%#v, \n\ttx=%#v, \n\tops=%+v", feeBumpTx, feeBumpTx.InnerTransaction(), opsSliceStr(feeBumpTx.InnerTransaction().Operations())), nil
-	}
-
-	return fmt.Sprintf("%+v", genericTx), nil
-}
-
 func NewIntegrationTests(ctx context.Context, opts IntegrationTestsOptions) (*IntegrationTests, error) {
 	if err := opts.Validate(); err != nil {
 		return nil, fmt.Errorf("validating integration tests options: %w", err)
