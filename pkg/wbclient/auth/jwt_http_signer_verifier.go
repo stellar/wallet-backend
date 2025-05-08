8fe9e978eddd588a04b8ca28c7e8670e3d620761
Marcelo Salloum <marcelo@stellar.org>
2025-05-08 13:13:35 -0700

    Create dedicated http request signer and verifier.

---
+++ pkg/wbclient/auth/jwt_http_signer_verifier.go
@@ -0,0 +1,117 @@
+package auth
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const DefaultMaxBodySize int64 = 10_240 // 10kb
+
+var ErrUnauthorized = errors.New("not authorized")
+
+// HTTPRequestSigner is responsible for signing HTTP requests using JWTs.
+type HTTPRequestSigner interface {
+	SignHTTPRequest(req *http.Request, timeout time.Duration) error
+}
+
+// HTTPRequestVerifier is responsible for verifying HTTP requests using JWTs.
+type HTTPRequestVerifier interface {
+	VerifyHTTPRequest(req *http.Request, audience string) error
+}
+
+// JWTHTTPSignerVerifier implements both signing and verifying of HTTP requests.
+type JWTHTTPSignerVerifier struct {
+	parser      JWTTokenParser
+	generator   JWTTokenGenerator
+	maxBodySize int64
+}
+
+func (s *JWTHTTPSignerVerifier) MaxBodySize() int64 {
+	if s.maxBodySize == 0 {
+		return DefaultMaxBodySize
+	}
+	return s.maxBodySize
+}
+
+// SignHTTPRequest signs an HTTP request with a JWT.
+func (s *JWTHTTPSignerVerifier) SignHTTPRequest(req *http.Request, timeout time.Duration) error {
+	// Read the request body
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return fmt.Errorf("reading request body: %w", err)
+	}
+	defer func() { // Reset the body so it can be read again
+		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	}()
+
+	// Parse the hostname
+	u, err := url.ParseRequestURI(req.URL.Scheme + "://" + req.Host)
+	if err != nil {
+		return fmt.Errorf("parsing hostname: %w", err)
+	}
+
+	// Generate the token and sign the request
+	jwtToken, err := s.generator.GenerateJWT(u.Hostname(), bodyBytes, time.Now().Add(timeout))
+	if err != nil {
+		return fmt.Errorf("generating JWT token: %w", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", jwtToken))
+
+	return nil
+}
+
+// VerifyHTTPRequest verifies the JWT in an HTTP request.
+func (s *JWTHTTPSignerVerifier) VerifyHTTPRequest(req *http.Request, audience string) error {
+	authHeader := req.Header.Get("Authorization")
+	if authHeader == "" {
+		return fmt.Errorf("missing Authorization header: %w", ErrUnauthorized)
+	}
+
+	// check if the Authorization header has two parts ['Bearer', token]
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		return fmt.Errorf("the Authorization header is invalid, expected 'Bearer <token>': %w", ErrUnauthorized)
+	}
+
+	// Read the request body
+	var bodyBytes []byte
+	if req.Body != nil {
+		var err error
+		if bodyBytes, err = io.ReadAll(io.LimitReader(req.Body, s.MaxBodySize())); err != nil {
+			return fmt.Errorf("reading request body: %w", err)
+		} else {
+			defer func() { // Reset the body so it can be read again
+				req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+			}()
+		}
+	}
+
+	// Parse the JWT
+	tokenString := authHeader[len("Bearer "):] // Remove "Bearer " prefix
+	_, _, err := s.parser.ParseJWT(tokenString, audience, bodyBytes)
+	if err != nil {
+		return fmt.Errorf("verifying JWT: %w: %w", err, ErrUnauthorized)
+	}
+
+	return nil
+}
+
+// NewHTTPRequestSigner creates a new HTTPRequestSigner with the given JWTTokenGenerator.
+func NewHTTPRequestSigner(generator JWTTokenGenerator) HTTPRequestSigner {
+	return &JWTHTTPSignerVerifier{
+		generator: generator,
+	}
+}
+
+// NewHTTPRequestVerifier creates a new HTTPRequestVerifier with the given JWTTokenParser.
+func NewHTTPRequestVerifier(parser JWTTokenParser, maxBodySize int64) HTTPRequestVerifier {
+	return &JWTHTTPSignerVerifier{
+		parser:      parser,
+		maxBodySize: maxBodySize,
+	}
+}
---
+++ pkg/wbclient/auth/jwt_http_signer_verifier_test.go
@@ -0,0 +1,135 @@
+package auth
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_JWTHTTPSignerVerifier_Integration(t *testing.T) {
+	reqBodyTooBig := make([]byte, DefaultMaxBodySize*2)
+
+	jwtParser, err := NewJWTTokenParser(10*time.Second, testKP1.Address())
+	require.NoError(t, err)
+	reqJWTVerifier := NewHTTPRequestVerifier(jwtParser, DefaultMaxBodySize)
+
+	validJWTGenerator, err := NewJWTTokenGenerator(testKP1.Seed())
+	require.NoError(t, err)
+	validSigner := NewHTTPRequestSigner(validJWTGenerator)
+	invalidJWTGenerator, err := NewJWTTokenGenerator(testKP2.Seed())
+	require.NoError(t, err)
+	invalidSigner := NewHTTPRequestSigner(invalidJWTGenerator)
+
+	testCases := []struct {
+		name            string
+		setupRequest    func(t *testing.T) *http.Request
+		expectedStatus  int
+		wantErrContains string
+	}{
+		{
+			name: "🔴missing_authorization_header",
+			setupRequest: func(t *testing.T) *http.Request {
+				return httptest.NewRequest("GET", "http://example.com/authenticated", nil)
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			wantErrContains: "missing Authorization header",
+		},
+		{
+			name: "🔴invalid_authorization_header",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", nil)
+				req.Header.Set("Authorization", "invalid-token")
+				return req
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			wantErrContains: "the Authorization header is invalid, expected 'Bearer <token>'",
+		},
+		{
+			name: "🔴invalid_bearer_token",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", nil)
+				req.Header.Set("Authorization", "Bearer invalid-bearer")
+				return req
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			wantErrContains: "verifying JWT",
+		},
+		{
+			name: "🔴wrong_signer",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", nil)
+				err := invalidSigner.SignHTTPRequest(req, time.Second*5)
+				require.NoError(t, err)
+				return req
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			wantErrContains: "verifying JWT",
+		},
+		{
+			name: "🔴body_is_too_big",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", bytes.NewBuffer(reqBodyTooBig))
+				err := validSigner.SignHTTPRequest(req, time.Second*5)
+				require.NoError(t, err)
+				return req
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			wantErrContains: "verifying JWT: the claims' hashed body does not match the request body's hash",
+		},
+		{
+			name: "🟢valid_authenticated_request_no_body",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", nil)
+				err := validSigner.SignHTTPRequest(req, time.Second*5)
+				require.NoError(t, err)
+				return req
+			},
+			expectedStatus:  http.StatusOK,
+			wantErrContains: `{"message": "ok"}`,
+		},
+		{
+			name: "🟢valid_authenticated_request_with_body",
+			setupRequest: func(t *testing.T) *http.Request {
+				req := httptest.NewRequest("GET", "http://example.com/authenticated", bytes.NewBuffer([]byte(`{"foo": "bar"}`)))
+				err := validSigner.SignHTTPRequest(req, time.Second*5)
+				require.NoError(t, err)
+				return req
+			},
+			expectedStatus:  http.StatusOK,
+			wantErrContains: `{"message": "ok"}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := tc.setupRequest(t)
+			w := httptest.NewRecorder()
+
+			ts := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				err := reqJWTVerifier.VerifyHTTPRequest(r, "example.com")
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusUnauthorized)
+					return
+				}
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write([]byte(`{"message": "ok"}`))
+				require.NoError(t, err)
+			})
+
+			ts.ServeHTTP(w, req)
+
+			resp := w.Result()
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.expectedStatus, resp.StatusCode)
+			assert.Contains(t, string(respBody), tc.wantErrContains)
+		})
+	}
+}
