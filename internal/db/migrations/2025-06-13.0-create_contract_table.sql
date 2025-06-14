d313932dbb1fff524a2a93b61d6a05837157934d
Aditya Vyas <aditya.vyas@stellar.org>
2025-06-14 19:09:03 -0400

    add migration for contracts table

---
+++ internal/db/migrations/2025-06-13.0-create_contract_table.sql
@@ -0,0 +1,14 @@
+-- +migrate Up
+
+-- Table: contracts
+CREATE TABLE contracts (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- +migrate Down
+
+DROP TABLE contracts CASCADE;
