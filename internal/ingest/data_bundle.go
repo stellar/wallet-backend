29e83b268c3bf58bd10230c39f45fd7f68542403
Marcelo Salloum <marcelo@stellar.org>
2025-06-17 10:53:48 -0700

    Start tracking participants of a ledger transaction

---
+++ internal/ingest/data_bundle.go
@@ -0,0 +1,88 @@
+package ingest
+
+import "github.com/stellar/wallet-backend/internal/indexer/types"
+
+type Participant string
+
+// Set is a set of strings that enforces uniqueness
+type Set[T comparable] map[T]struct{}
+
+// Add adds a value to the set
+func (s Set[T]) Add(value T) {
+	if s == nil {
+		s = make(Set[T])
+	}
+	s[value] = struct{}{}
+}
+
+// Contains checks if a value exists in the set
+func (s Set[T]) Contains(value T) bool {
+	_, exists := s[value]
+	return exists
+}
+
+// ToSlice converts the set to a slice
+func (s Set[T]) ToSlice() []T {
+	result := make([]T, 0, len(s))
+	for k := range s {
+		result = append(result, k)
+	}
+	return result
+}
+
+type DataBundle struct {
+	Network               string
+	Participants          Set[Participant]
+	OpByID                map[string]types.Operation
+	TxByHash              map[string]types.Transaction
+	TxHashesByParticipant map[Participant]Set[string]
+	OpIDsByParticipant    map[Participant]Set[string]
+}
+
+func NewDataBundle(network string) DataBundle {
+	return DataBundle{
+		Network:               network,
+		Participants:          Set[Participant]{},
+		OpByID:                map[string]types.Operation{},
+		TxByHash:              map[string]types.Transaction{},
+		TxHashesByParticipant: map[Participant]Set[string]{},
+		OpIDsByParticipant:    map[Participant]Set[string]{},
+	}
+}
+
+func (b *DataBundle) PushTransactionWithParticipant(participant Participant, transaction types.Transaction) {
+	b.TxByHash[transaction.Hash] = transaction
+	b.TxHashesByParticipant[participant].Add(transaction.Hash)
+	b.Participants.Add(participant)
+}
+
+func (b *DataBundle) PushOperationWithParticipant(participant Participant, operation types.Operation) {
+	b.OpByID[operation.ID] = operation
+	b.OpIDsByParticipant[participant].Add(operation.ID)
+	b.TxHashesByParticipant[participant].Add(operation.TxHash)
+	b.Participants.Add(participant)
+}
+
+func (b *DataBundle) GetParticipantOperationIDs(participant Participant) Set[string] {
+	return b.OpIDsByParticipant[participant]
+}
+
+func (b *DataBundle) GetParticipantOperations(participant Participant) []types.Operation {
+	ops := []types.Operation{}
+	for _, opID := range b.OpIDsByParticipant[participant].ToSlice() {
+		ops = append(ops, b.OpByID[opID])
+	}
+	return ops
+}
+
+func (b *DataBundle) GetParticipantTransactionHashes(participant Participant) Set[string] {
+	return b.TxHashesByParticipant[participant]
+}
+
+func (b *DataBundle) GetParticipantTransactions(participant Participant) []types.Transaction {
+	txs := []types.Transaction{}
+	for _, txHash := range b.TxHashesByParticipant[participant].ToSlice() {
+		txs = append(txs, b.TxByHash[txHash])
+	}
+	return txs
+}
--- internal/ingest/participants.go
+++ internal/ingest/participants.go
@@ -2,7 +2,6 @@ package ingest
 
 import (
 	"fmt"
-	"sort"
 	"time"
 
 	"github.com/stellar/go/ingest"
@@ -16,16 +15,14 @@ import (
 // ParticipantsProcessor is a processor which ingests various participants
 // from different sources (transactions, operations, etc)
 type ParticipantsProcessor struct {
-	TxsByParticipant map[Participant][]types.Transaction
-	OpsByParticipant map[Participant][]types.Operation
-	network          string
+	network    string
+	dataBundle DataBundle
 }
 
 func NewParticipantsProcessor(network string) *ParticipantsProcessor {
 	return &ParticipantsProcessor{
-		network:          network,
-		TxsByParticipant: make(map[Participant][]types.Transaction),
-		OpsByParticipant: make(map[Participant][]types.Operation),
+		network:    network,
+		dataBundle: NewDataBundle(network),
 	}
 }
 
@@ -91,66 +88,87 @@ func participantsForMeta(meta xdr.TransactionMeta) ([]xdr.AccountId, error) {
 	return participants, nil
 }
 
-func (p *ParticipantsProcessor) addTransactionParticipants(
-	sequence uint32,
-	transaction ingest.LedgerTransaction,
-) error {
-	if len(p.OpsByParticipant) == 0 {
-		err := p.addOperationsParticipants(sequence, transaction)
-		if err != nil {
-			return fmt.Errorf("could not determine operation participants: %w", err)
-		}
+func (p *ParticipantsProcessor) addTransactionParticipants(transaction ingest.LedgerTransaction) error {
+	// 1. Get direct participants involved in the transaction
+	participants := []xdr.AccountId{
+		transaction.Envelope.SourceAccount().ToAccountId(),
+	}
+	if transaction.Envelope.IsFeeBump() {
+		participants = append(participants, transaction.Envelope.FeeBumpAccount().ToAccountId())
 	}
 
-	txsByParticipant, err := participantsForTransaction(sequence, transaction, p.OpsByParticipant)
+	metaParticipants, err := participantsForMeta(transaction.UnsafeMeta)
 	if err != nil {
-		return fmt.Errorf("could not determine participants for transaction: %w", err)
+		return fmt.Errorf("identifying participants for meta: %w", err)
 	}
-	p.TxsByParticipant = txsByParticipant
+	participants = append(participants, metaParticipants...)
+
+	feeParticipants, err := participantsForChanges(transaction.FeeChanges)
+	if err != nil {
+		return fmt.Errorf("identifying participants for changes: %w", err)
+	}
+	participants = append(participants, feeParticipants...)
+
+	// 2. Build database transaction object
+	envelopeXDR, err := xdr.MarshalBase64(transaction.Envelope)
+	if err != nil {
+		return fmt.Errorf("marshalling transaction envelope: %w", err)
+	}
+
+	resultXDR, err := xdr.MarshalBase64(transaction.Result)
+	if err != nil {
+		return fmt.Errorf("marshalling transaction result: %w", err)
+	}
+
+	metaXDR, err := xdr.MarshalBase64(transaction.UnsafeMeta)
+	if err != nil {
+		return fmt.Errorf("marshalling transaction meta: %w", err)
+	}
+
+	dbTx := types.Transaction{
+		Hash:            transaction.Hash.HexString(),
+		LedgerCreatedAt: transaction.Ledger.ClosedAt(),
+		IngestedAt:      time.Now(),
+		EnvelopeXDR:     envelopeXDR,
+		ResultXDR:       resultXDR,
+		MetaXDR:         metaXDR,
+		LedgerNumber:    transaction.LedgerVersion,
+	}
+
+	// 3. Push transaction and participants to data bundle
+	for _, xdrParticipant := range participants {
+		p.dataBundle.PushTransactionWithParticipant(Participant(xdrParticipant.Address()), dbTx)
+	}
 
 	return nil
 }
 
-func (p *ParticipantsProcessor) addOperationsParticipants(
-	sequence uint32,
-	transaction ingest.LedgerTransaction,
-) error {
-	participants, err := operationsParticipants(transaction, sequence, p.network)
-	if err != nil {
-		return errors.Wrap(err, "could not determine operation participants")
-	}
-
-	p.OpsByParticipant = participants
-
-	return nil
-}
-
-type Participant string
-
-// operationsParticipants returns a map with all participants per operation.
-func operationsParticipants(transaction ingest.LedgerTransaction, sequence uint32, network string) (allOpsParticipants map[Participant][]types.Operation, err error) {
+func (p *ParticipantsProcessor) addOperationsParticipants(sequence uint32, transaction ingest.LedgerTransaction) error {
 	now := time.Now()
 	ledgerCreatedAt := transaction.Ledger.ClosedAt()
 	txHash := transaction.Hash.HexString()
 
 	for opi, xdrOp := range transaction.Envelope.Operations() {
+		// 1. Build op wrapper, so we can use its methods
 		op := operation_processor.TransactionOperationWrapper{
 			Index:          uint32(opi),
 			Transaction:    transaction,
 			Operation:      xdrOp,
 			LedgerSequence: sequence,
-			Network:        network,
+			Network:        p.network,
 		}
 		opID := op.ID()
 
-		p, err := op.Participants()
+		// 2. Get participants for the operation
+		participants, err := op.Participants()
 		if err != nil {
-			return allOpsParticipants, errors.Wrapf(err, "reading operation %v participants", opID)
+			return fmt.Errorf("reading operation %v participants: %w", opID, err)
 		}
 
+		// 3. Build database operation object
 		xdrOpStr, err := xdr.MarshalBase64(xdrOp)
 		if err != nil {
-			return allOpsParticipants, fmt.Errorf("marshalling operation %v: %w", opID, err)
+			return fmt.Errorf("marshalling operation %v: %w", opID, err)
 		}
 		dbOp := types.Operation{
 			ID:              string(opID),
@@ -161,13 +179,13 @@ func operationsParticipants(transaction ingest.LedgerTransaction, sequence uint3
 			TxHash:          txHash,
 		}
 
-		for _, xdrParticipant := range dedupeParticipants(p) {
-			participant := Participant(xdrParticipant.Address())
-			allOpsParticipants[participant] = append(allOpsParticipants[participant], dbOp)
+		// 4. Push operation and participants to data bundle
+		for _, xdrParticipant := range participants {
+			p.dataBundle.PushOperationWithParticipant(Participant(xdrParticipant.Address()), dbOp)
 		}
 	}
 
-	return allOpsParticipants, nil
+	return nil
 }
 
 func (p *ParticipantsProcessor) ProcessTransaction(lcm xdr.LedgerCloseMeta, transaction ingest.LedgerTransaction) error {
@@ -175,133 +193,9 @@ func (p *ParticipantsProcessor) ProcessTransaction(lcm xdr.LedgerCloseMeta, tran
 		return err
 	}
 
-	if err := p.addTransactionParticipants(lcm.LedgerSequence(), transaction); err != nil {
+	if err := p.addTransactionParticipants(transaction); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func directParticipantsForTransaction(transaction ingest.LedgerTransaction) (participantStrs []Participant, err error) {
-	participants := []xdr.AccountId{
-		transaction.Envelope.SourceAccount().ToAccountId(),
-	}
-	if transaction.Envelope.IsFeeBump() {
-		participants = append(participants, transaction.Envelope.FeeBumpAccount().ToAccountId())
-	}
-
-	p, err := participantsForMeta(transaction.UnsafeMeta)
-	if err != nil {
-		return nil, fmt.Errorf("identifying participants for meta: %w", err)
-	}
-	participants = append(participants, p...)
-
-	p, err = participantsForChanges(transaction.FeeChanges)
-	if err != nil {
-		return nil, fmt.Errorf("identifying participants for changes: %w", err)
-	}
-	participants = append(participants, p...)
-	participants = dedupeParticipants(participants)
-
-	participantStrs = make([]Participant, len(participants))
-	for _, p := range participants {
-		participantStrs = append(participantStrs, Participant(p.Address()))
-	}
-
-	return participantStrs, nil
-}
-
-func participantsForTransaction(
-	sequence uint32,
-	transaction ingest.LedgerTransaction,
-	opsByParticipant map[Participant][]types.Operation,
-) (txsByParticipant map[Participant][]types.Transaction, err error) {
-	txDirectParticipants, err := directParticipantsForTransaction(transaction)
-	if err != nil {
-		return nil, fmt.Errorf("could not determine participants for transaction: %w", err)
-	}
-
-	opsParticipants := make([]Participant, 0, len(opsByParticipant))
-	for k := range opsByParticipant {
-		opsParticipants = append(opsParticipants, k)
-	}
-
-	participants := make([]Participant, 0, len(txDirectParticipants)+len(opsParticipants))
-	participants = append(participants, txDirectParticipants...)
-	participants = append(participants, opsParticipants...)
-	participants = DedupeComparable(participants)
-
-	envelopeXDR, err := xdr.MarshalBase64(transaction.Envelope)
-	if err != nil {
-		return nil, errors.Wrapf(err, "marshalling transaction envelope")
-	}
-
-	resultXDR, err := xdr.MarshalBase64(transaction.Result)
-	if err != nil {
-		return nil, errors.Wrapf(err, "marshalling transaction result")
-	}
-
-	metaXDR, err := xdr.MarshalBase64(transaction.UnsafeMeta)
-	if err != nil {
-		return nil, errors.Wrapf(err, "marshalling transaction meta")
-	}
-
-	now := time.Now()
-	txDB := types.Transaction{
-		Hash:            transaction.Hash.HexString(),
-		LedgerCreatedAt: transaction.Ledger.ClosedAt(),
-		IngestedAt:      now,
-		EnvelopeXDR:     envelopeXDR,
-		ResultXDR:       resultXDR,
-		MetaXDR:         metaXDR,
-		LedgerNumber:    transaction.LedgerVersion,
-	}
-	for _, participant := range participants {
-		txsByParticipant[participant] = append(txsByParticipant[participant], txDB)
-	}
-
-	return txsByParticipant, nil
-}
-
-// dedupeParticipants remove any duplicate ids from `in`
-func dedupeParticipants(in []xdr.AccountId) []xdr.AccountId {
-	if len(in) <= 1 {
-		return in
-	}
-	sort.Slice(in, func(i, j int) bool {
-		return in[i].Address() < in[j].Address()
-	})
-	insert := 1
-	for cur := 1; cur < len(in); cur++ {
-		if in[cur].Equals(in[cur-1]) {
-			continue
-		}
-		if insert != cur {
-			in[insert] = in[cur]
-		}
-		insert++
-	}
-	return in[:insert]
-}
-
-// Dedupe removes duplicate elements from a slice while preserving order.
-// It works with any comparable type.
-func DedupeComparable[T comparable](slice []T) []T {
-	if len(slice) == 0 {
-		return slice
-	}
-
-	// Create a map to track seen elements
-	seen := make(map[T]struct{}, len(slice))
-	result := make([]T, 0, len(slice))
-
-	// Iterate through the slice and only add elements we haven't seen before
-	for _, item := range slice {
-		if _, exists := seen[item]; !exists {
-			seen[item] = struct{}{}
-			result = append(result, item)
-		}
-	}
-
-	return result
 }
