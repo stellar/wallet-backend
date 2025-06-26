6ef653e4eea87d9797658d3849707eca1cc2b16e
Marcelo Salloum <marcelo@stellar.org>
2025-06-26 09:51:13 -0700

    Start finding participants in operations

--- internal/processors/helpers.go
+++ internal/processors/helpers.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stellar/wallet-backend/internal/indexer/types"
 )
 
-func ConvertTransaction(transaction ingest.LedgerTransaction) (*types.Transaction, error) {
+func ConvertTransaction(transaction *ingest.LedgerTransaction) (*types.Transaction, error) {
 	envelopeXDR, err := xdr.MarshalBase64(transaction.Envelope)
 	if err != nil {
 		return nil, fmt.Errorf("marshalling transaction envelope: %w", err)
@@ -37,5 +37,20 @@ func ConvertTransaction(transaction ingest.LedgerTransaction) (*types.Transactio
 		ResultXDR:       resultXDR,
 		MetaXDR:         metaXDR,
 		LedgerNumber:    ledgerSequence,
+	}, nil
+}
+
+func ConvertOperation(transaction *ingest.LedgerTransaction, op *xdr.Operation, opID int64) (*types.Operation, error) {
+	xdrOpStr, err := xdr.MarshalBase64(op)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling operation %d: %w", opID, err)
+	}
+
+	return &types.Operation{
+		ID:              opID,
+		OperationType:   types.OperationTypeFromXDR(op.Body.Type),
+		OperationXDR:    xdrOpStr,
+		LedgerCreatedAt: transaction.Ledger.ClosedAt(),
+		TxHash:          transaction.Hash.HexString(),
 	}, nil
 }
