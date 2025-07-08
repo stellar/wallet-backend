24003c49f308e0d338c18eb2a038b30b552b560f
Marcelo Salloum <marcelo@stellar.org>
2025-07-08 13:21:02 -0700

    Stop using `LedgerTransaction.ContractIdFromTxEnvelope()`, as the team mentioned this method is bugged and will go away soon.

--- internal/processors/contract_operations.go
+++ internal/processors/contract_operations.go
@@ -1,10 +1,12 @@
 package processors
 
 import (
+	"crypto/sha256"
 	"errors"
 	"fmt"
 
 	set "github.com/deckarep/golang-set/v2"
+	"github.com/stellar/go/ingest"
 	operation_processor "github.com/stellar/go/processors/operation"
 	"github.com/stellar/go/strkey"
 	"github.com/stellar/go/xdr"
@@ -14,28 +16,124 @@ import (
 
 var ErrNotSorobanOperation = errors.New("not a soroban operation")
 
+// calculateContractID calculates the contract ID for a wallet creation transaction based on the network passphrase, deployer account and salt.
+//
+// More info: https://developers.stellar.org/docs/build/smart-contracts/example-contracts/deployer#how-it-works
+func calculateContractID(networkPassphrase string, fromAddress xdr.ContractIdPreimageFromAddress) (string, error) {
+	networkHash := xdr.Hash(sha256.Sum256([]byte(networkPassphrase)))
+
+	hashIDPreimage := xdr.HashIdPreimage{
+		Type: xdr.EnvelopeTypeEnvelopeTypeContractId,
+		ContractId: &xdr.HashIdPreimageContractId{
+			NetworkId: networkHash,
+			ContractIdPreimage: xdr.ContractIdPreimage{
+				Type:        xdr.ContractIdPreimageTypeContractIdPreimageFromAddress,
+				FromAddress: &fromAddress,
+			},
+		},
+	}
+
+	preimageXDR, err := hashIDPreimage.MarshalBinary()
+	if err != nil {
+		return "", fmt.Errorf("marshaling preimage: %w", err)
+	}
+
+	contractIDHash := sha256.Sum256(preimageXDR)
+	contractID, err := strkey.Encode(strkey.VersionByteContract, contractIDHash[:])
+	if err != nil {
+		return "", fmt.Errorf("encoding contract ID: %w", err)
+	}
+
+	return contractID, nil
+}
+
+// contractIDForInvokeHostFunctionOp returns the contract ID for an InvokeHostFunction operation:
+// - It returns an error ErrNotSorobanOperation if the operation is not a Soroban operation.
+// - It returns the host's contract ID for InvokeContract operations.
+// - It calculates the contractID based on the network, salt and deployer, for CreateContract (V1 & V2) operations.
+func contractIDForInvokeHostFunctionOp(networkPassphrase string, invokeHostFunctionOp xdr.InvokeHostFunctionOp) (string, error) {
+	switch invokeHostFunctionOp.HostFunction.Type {
+	case xdr.HostFunctionTypeHostFunctionTypeInvokeContract: // InvokeHostFunction->InvokeContract
+		contractIDHash := invokeHostFunctionOp.HostFunction.MustInvokeContract().ContractAddress.ContractId
+		return strkey.MustEncode(strkey.VersionByteContract, contractIDHash[:]), nil
+
+	case xdr.HostFunctionTypeHostFunctionTypeCreateContract, xdr.HostFunctionTypeHostFunctionTypeCreateContractV2:
+		var preimage xdr.ContractIdPreimage
+		switch invokeHostFunctionOp.HostFunction.Type {
+		case xdr.HostFunctionTypeHostFunctionTypeCreateContract:
+			preimage = invokeHostFunctionOp.HostFunction.MustCreateContract().ContractIdPreimage
+		case xdr.HostFunctionTypeHostFunctionTypeCreateContractV2:
+			preimage = invokeHostFunctionOp.HostFunction.MustCreateContractV2().ContractIdPreimage
+		default:
+			return "", nil
+		}
+
+		if preimage.Type == xdr.ContractIdPreimageTypeContractIdPreimageFromAddress {
+			return calculateContractID(networkPassphrase, preimage.MustFromAddress())
+		}
+
+	default:
+		break
+	}
+
+	return "", nil
+}
+
+// contractIDFromContractData returns the contract ID for a contract data ledger key.
+// It returns an empty string and false if the ledger key is not a contract data ledger key.
+func contractIDFromContractData(ledgerKey xdr.LedgerKey) (string, bool) {
+	contractData, ok := ledgerKey.GetContractData()
+	if !ok {
+		return "", false
+	}
+	contractIDHash, ok := contractData.Contract.GetContractId()
+	if !ok {
+		return "", false
+	}
+
+	contractIDByte, err := contractIDHash.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+
+	return strkey.MustEncode(strkey.VersionByteContract, contractIDByte), true
+}
+
+// contractIDsFromTxEnvelope returns the contract IDs for a transaction envelope.
+// It returns an empty string if the transaction envelope is not a Soroban transaction envelope.
+func contractIDsFromTxEnvelope(t *ingest.LedgerTransaction) string {
+	v1Envelope, ok := t.GetTransactionV1Envelope()
+	if !ok {
+		return ""
+	}
+
+	readWrite := v1Envelope.Tx.Ext.SorobanData.Resources.Footprint.ReadWrite
+	readOnly := v1Envelope.Tx.Ext.SorobanData.Resources.Footprint.ReadOnly
+	for _, ledgerKey := range append(readWrite, readOnly...) {
+		contractID, ok := contractIDFromContractData(ledgerKey)
+		if ok && contractID != "" {
+			return contractID
+		}
+	}
+
+	return ""
+}
+
 func contractIDForSorobanOperation(op operation_processor.TransactionOperationWrapper) (string, error) {
 	if !op.Transaction.IsSorobanTx() {
 		return "", ErrNotSorobanOperation
 	}
 
-	// 👋 This shouldn't be needed, but it's a workaround for a potential bug where `ContractIdFromTxEnvelope` returns
-	// an empty string for when there are a mix of Soroban and non-Soroban ledger entries. More info in the internal
-	// discussion: https://stellarfoundation.slack.com/archives/C02B04RMK/p1751935606699499.
-	if op.Operation.Body.Type == xdr.OperationTypeInvokeHostFunction {
+	switch op.Operation.Body.Type {
+	case xdr.OperationTypeInvokeHostFunction:
 		invokeHostOp := op.Operation.Body.MustInvokeHostFunctionOp()
-		if invokeHostOp.HostFunction.Type == xdr.HostFunctionTypeHostFunctionTypeInvokeContract {
-			invokeContract := invokeHostOp.HostFunction.MustInvokeContract()
-			contractIDHash := invokeContract.ContractAddress.ContractId
-			if contractIDHash != nil {
-				return strkey.MustEncode(strkey.VersionByteContract, contractIDHash[:]), nil
-			}
-		}
-	}
+		return contractIDForInvokeHostFunctionOp(op.Network, invokeHostOp)
 
-	contractID, ok := op.Transaction.ContractIdFromTxEnvelope()
-	if ok && contractID != "" {
-		return contractID, nil
+	case xdr.OperationTypeExtendFootprintTtl, xdr.OperationTypeRestoreFootprint:
+		return contractIDsFromTxEnvelope(&op.Transaction), nil
+
+	default:
+		break
 	}
 
 	return "", nil
