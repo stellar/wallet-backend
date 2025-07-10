b64cf75e0bd16ed56bc80858c1a27660769aeda7
Aditya Vyas <aditya.vyas@stellar.org>
2025-07-10 10:12:40 -0400

    change package name to resolvers

--- internal/serve/graphql/resolvers/transaction.resolvers.go
+++ internal/serve/graphql/resolvers/transaction.resolvers.go
@@ -1,4 +1,4 @@
-package graphql
+package resolvers
 
 // This file will be automatically regenerated based on the schema, any resolver implementations
 // will be copied through when generating and any unknown code will be moved to the end.
