61821ea3ea7f93e8a2abcff6e918b8d6a5d93201
Aditya Vyas <aditya.vyas@stellar.org>
2025-07-18 12:43:26 -0400

    Add some comment explaining the per request data loaders

--- internal/serve/middleware/dataloader_middleware.go
+++ internal/serve/middleware/dataloader_middleware.go
@@ -17,6 +17,10 @@ const (
 func DataloaderMiddleware(models *data.Models) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Dataloaders are created per-request to avoid data sharing between requests.
+			// This ensures each request has a fresh view of the data. This also helps prevent
+			// inconsistent data view across horizontally scaled services.
+			// More info about this here: https://github.com/graphql/dataloader/issues/62#issue-193854091
 			loaders := dataloaders.NewDataloaders(models)
 			ctx := context.WithValue(r.Context(), LoadersKey, loaders)
 			next.ServeHTTP(w, r.WithContext(ctx))
