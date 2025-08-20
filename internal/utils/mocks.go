6e6d6037d6c9ca064b9754f019e4b855ad986819
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2025-08-20 09:13:11 -0700

    Bugfix: race condition caused by processing taking too long (#295)
    ### What
    
    Only call the ingestor's manualTriggerChannel if it's empty
    
    ### Why
    
    When the processing time takes longer than 5s, which is common in production, since even downloading the ledgers from the RPC takes ~5s for 10 ledgers, the code was entering a deadlock.
    
    Considering the processing time is 11s, here's the sequence of events that lead to a deadlock:
    
    - [**T=0s**] RPC Health Check: channel now contains 1 health update
    https://github.com/stellar/wallet-backend/blob/8a12000c478e72169413ef2b47085415b5c1fdb7/internal/services/rpc_service.go#L284
    - [**T=0s**] Ingestion Loop Starts. Channel will be emptied and the processing step will take 11s
    https://github.com/stellar/wallet-backend/blob/8a12000c478e72169413ef2b47085415b5c1fdb7/internal/services/ingest.go#L226
    - [**T=5s**] RPC Health Check (BLOCKS!): RPC health goroutine STOPS executing since it gets blocked until `r.heartbeatChannel` is consumed.
    https://github.com/stellar/wallet-backend/blob/8a12000c478e72169413ef2b47085415b5c1fdb7/internal/services/rpc_service.go#L284
    - [**T=11s**]: Ingestion Loop Finishes (BLOCKS!): the manual trigger is called, but since the RPC health check is already halted, this will make the ingestor halt as well, resulting in a deadlock.

--- internal/utils/mocks.go
+++ internal/utils/mocks.go
@@ -13,8 +13,19 @@ type MockHTTPClient struct {
 
 func (s *MockHTTPClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
 	args := s.Called(url, contentType, body)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
+
+	// Handle function return values (like Mockery does)
+	if args.Get(0) != nil {
+		if returnFunc, ok := args.Get(0).(func(string, string, io.Reader) *http.Response); ok {
+			resp = returnFunc(url, contentType, body)
+		} else {
+			resp = args.Get(0).(*http.Response)
+		}
 	}
-	return args.Get(0).(*http.Response), args.Error(1)
+
+	if args.Error(1) != nil {
+		err = args.Error(1)
+	}
+
+	return resp, err
 }
