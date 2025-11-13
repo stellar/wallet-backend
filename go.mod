7f8459f811a5fa2fe03080dce45bd43d34429ed7
Aditya Vyas <aditya.vyas@stellar.org>
2025-11-13 11:48:37 -0500

    Add redis store

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.24.2
 
 require (
 	github.com/99designs/gqlgen v0.17.76
+	github.com/alicebob/miniredis/v2 v2.35.0
 	github.com/alitto/pond/v2 v2.5.0
 	github.com/avast/retry-go/v4 v4.6.1
 	github.com/aws/aws-sdk-go v1.55.7
@@ -22,6 +23,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.28
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/prometheus/client_golang v1.22.0
+	github.com/redis/go-redis/v9 v9.16.0
 	github.com/rubenv/sql-migrate v1.8.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.9.1
@@ -86,6 +88,7 @@ require (
 	github.com/creachadair/mds v0.13.4 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-farm v0.0.0-20240924180020-3414d57e47da // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/djherbis/fscache v0.10.1 // indirect
 	github.com/docker/docker v28.0.1+incompatible // indirect
@@ -158,6 +161,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	github.com/zeebo/errs v1.4.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
---
+++ internal/store/redis.go
@@ -0,0 +1,110 @@
+// Package store provides data access layer for Redis operations.
+package store
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// RedisOperation represents a single Redis set operation type.
+type RedisOperation string
+
+const (
+	SetOpAdd    RedisOperation = "SADD"
+	SetOpRemove RedisOperation = "SREM"
+	OpSet       RedisOperation = "SET"
+)
+
+// RedisPipelineOperation represents a single set operation to be executed in a pipeline.
+type RedisPipelineOperation struct {
+	Op      RedisOperation
+	Key     string
+	Members []string
+	Value   string
+}
+
+// RedisStore provides generic Redis operations for caching.
+type RedisStore struct {
+	client *redis.Client
+}
+
+// NewRedisStore creates a new Redis store with the given connection parameters.
+func NewRedisStore(host string, port int, password string) *RedisStore {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	return &RedisStore{
+		client: redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: password,
+		}),
+	}
+}
+
+// SAdd adds one or more members to a set stored at key.
+func (r *RedisStore) SAdd(ctx context.Context, key string, members ...string) error {
+	if err := r.client.SAdd(ctx, key, members).Err(); err != nil {
+		return fmt.Errorf("adding members to set %s: %w", key, err)
+	}
+	return nil
+}
+
+// SMembers returns all members of the set stored at key.
+func (r *RedisStore) SMembers(ctx context.Context, key string) ([]string, error) {
+	members, err := r.client.SMembers(ctx, key).Result()
+	if err != nil {
+		return nil, fmt.Errorf("getting members of set %s: %w", key, err)
+	}
+	return members, nil
+}
+
+// Get retrieves the value of a key.
+func (r *RedisStore) Get(ctx context.Context, key string) (string, error) {
+	val, err := r.client.Get(ctx, key).Result()
+	if errors.Is(err, redis.Nil) {
+		return "", nil // Key doesn't exist
+	}
+	if err != nil {
+		return "", fmt.Errorf("getting key %s: %w", key, err)
+	}
+	return val, nil
+}
+
+// Set stores a string value at a key.
+func (r *RedisStore) Set(ctx context.Context, key, value string) error {
+	if err := r.client.Set(ctx, key, value, 0).Err(); err != nil {
+		return fmt.Errorf("setting key %s: %w", key, err)
+	}
+	return nil
+}
+
+// ExecutePipeline executes multiple operations (SADD/SREM/SET) in a single pipeline.
+// This reduces network round trips and improves performance when processing many operations.
+// Returns an error if any operation in the pipeline fails.
+func (r *RedisStore) ExecutePipeline(ctx context.Context, operations []RedisPipelineOperation) error {
+	if len(operations) == 0 {
+		return nil
+	}
+
+	pipe := r.client.Pipeline()
+	for _, op := range operations {
+		switch op.Op {
+		case SetOpAdd:
+			pipe.SAdd(ctx, op.Key, op.Members)
+		case SetOpRemove:
+			pipe.SRem(ctx, op.Key, op.Members)
+		case OpSet:
+			pipe.Set(ctx, op.Key, op.Value, 0)
+		default:
+			return fmt.Errorf("unsupported set operation: %s", op.Op)
+		}
+	}
+
+	_, err := pipe.Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("executing set pipeline: %w", err)
+	}
+
+	return nil
+}
