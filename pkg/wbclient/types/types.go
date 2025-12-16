5dca8b3c22944156f10ae1b33702f839253a300f
Aditya Vyas <aditya.vyas@stellar.org>
2025-12-16 16:01:17 -0500

    fix failing integration tests - 1

--- pkg/wbclient/types/types.go
+++ pkg/wbclient/types/types.go
@@ -221,7 +221,7 @@ type Transaction struct {
 // GraphQLTransaction represents a transaction from the GraphQL API
 type GraphQLTransaction struct {
 	Hash            string    `json:"hash"`
-	EnvelopeXdr     string    `json:"envelopeXdr"`
+	EnvelopeXdr     *string    `json:"envelopeXdr"`
 	ResultXdr       string    `json:"resultXdr"`
 	MetaXdr         *string   `json:"metaXdr,omitempty"`
 	LedgerNumber    uint32    `json:"ledgerNumber"`
