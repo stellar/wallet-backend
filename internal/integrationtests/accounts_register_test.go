a673114edb099384cbe3b29c2146f2b06c89c1fd
Aditya Vyas <aditya.vyas@stellar.org>
2025-12-17 15:05:34 -0500

    make check

--- internal/integrationtests/accounts_register_test.go
+++ internal/integrationtests/accounts_register_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/stellar/go/keypair"
+
 	"github.com/stellar/wallet-backend/internal/integrationtests/infrastructure"
 	"github.com/stellar/wallet-backend/pkg/wbclient/types"
 )
