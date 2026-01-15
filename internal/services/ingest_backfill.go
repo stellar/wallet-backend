0edb0079a2abe63138e328fbdbf5bc731020e959
Aditya Vyas <aditya.vyas@stellar.org>
2026-01-15 15:18:40 -0500

    Update ingest_backfill.go

--- internal/services/ingest_backfill.go
+++ internal/services/ingest_backfill.go
@@ -452,15 +452,9 @@ func (m *ingestService) processTokenChanges(
 ) error {
 	// Sort changes by (LedgerNumber, OperationID) to ensure proper ordering
 	sort.Slice(trustlineChanges, func(i, j int) bool {
-		if trustlineChanges[i].LedgerNumber != trustlineChanges[j].LedgerNumber {
-			return trustlineChanges[i].LedgerNumber < trustlineChanges[j].LedgerNumber
-		}
 		return trustlineChanges[i].OperationID < trustlineChanges[j].OperationID
 	})
 	sort.Slice(contractChanges, func(i, j int) bool {
-		if contractChanges[i].LedgerNumber != contractChanges[j].LedgerNumber {
-			return contractChanges[i].LedgerNumber < contractChanges[j].LedgerNumber
-		}
 		return contractChanges[i].OperationID < contractChanges[j].OperationID
 	})
 
