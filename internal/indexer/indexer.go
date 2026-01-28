62e3b0ededfdeb6a4624c0023e3682c1169c6465
Aditya Vyas <aditya.vyas@stellar.org>
2026-01-28 14:07:48 -0500

    remove unused network passphrase

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -81,7 +81,7 @@ func NewIndexer(networkPassphrase string, pool pond.Pool, metricsService process
 	return &Indexer{
 		participantsProcessor:  processors.NewParticipantsProcessor(networkPassphrase),
 		tokenTransferProcessor: processors.NewTokenTransferProcessor(networkPassphrase, metricsService),
-		trustlinesProcessor:    processors.NewTrustlinesProcessor(networkPassphrase, metricsService),
+		trustlinesProcessor:    processors.NewTrustlinesProcessor(metricsService),
 		processors: []OperationProcessorInterface{
 			processors.NewEffectsProcessor(networkPassphrase, metricsService),
 			processors.NewContractDeployProcessor(networkPassphrase, metricsService),
