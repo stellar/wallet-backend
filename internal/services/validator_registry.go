9bec90028ee9b6a1d35f3f3833a1b81dcc3c316a
Aristides Staffieri <aristides.staffieri@stellar.org>
2026-03-13 16:25:27 -0600

    Add unit tests for validator registry
      Tests RegisterValidator and GetValidator for registration, lookup
      of unknown protocols, and overwrite behavior. Also removes outdated
      init() references from comments since registration is dynamic.

--- internal/services/validator_registry.go
+++ internal/services/validator_registry.go
@@ -1,11 +1,9 @@
 package services
 
 // validatorRegistry holds factory functions keyed by protocol ID.
-// Validators register themselves via init() using RegisterValidator.
 var validatorRegistry = map[string]func() ProtocolValidator{}
 
 // RegisterValidator registers a validator factory for a protocol ID.
-// Called from init() in each validator file.
 func RegisterValidator(protocolID string, factory func() ProtocolValidator) {
 	validatorRegistry[protocolID] = factory
 }
