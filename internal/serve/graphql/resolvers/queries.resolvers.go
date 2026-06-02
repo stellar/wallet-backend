b28ee5e88995d44042b6c1d2f00626c2f1f6c261
Aditya Vyas <aditya.vyas@stellar.org>
2026-06-02 09:40:43 -0400

    make check

--- internal/serve/graphql/resolvers/queries.resolvers.go
+++ internal/serve/graphql/resolvers/queries.resolvers.go
@@ -10,11 +10,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/vektah/gqlparser/v2/gqlerror"
-
 	"github.com/stellar/wallet-backend/internal/indexer/types"
 	graphql1 "github.com/stellar/wallet-backend/internal/serve/graphql/generated"
 	"github.com/stellar/wallet-backend/internal/utils"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
 // TransactionByHash is the resolver for the transactionByHash field.
